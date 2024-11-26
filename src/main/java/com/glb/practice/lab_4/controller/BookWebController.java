5ecbea44a364ff7d555c391686b0b2eac3b571ee
Gleb <glebkabaev200307@gmail.com>
2024-11-26 13:14:39 +0300

    getBook() deleteBook() init

--- src/main/java/com/glb/practice/lab_4/controller/BookWebController.java
+++ src/main/java/com/glb/practice/lab_4/controller/BookWebController.java
@@ -1,11 +1,10 @@
 package com.glb.practice.lab_4.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.glb.practice.lab_4.model.Book;
 import com.glb.practice.lab_4.model.BookModel;
 
 import org.springframework.stereotype.Controller;
@@ -21,6 +20,15 @@ public class BookWebController {
         model.addAttribute("books", bookModel.get_k_n_shortList(0,30));
         return "books";
     }
-    
+    @GetMapping({"/{id}","/{id}/"})
+    public String getBook(@PathVariable int id,Model model) {
+        model.addAttribute("book", bookModel.getBookById(id));
+        return "book";
+    }
+    @DeleteMapping({"/delete/{id}","/delete/{id}/"})
+    public String deleteBook(@PathVariable int id){
+        bookModel.deleteBookById(id);
+        return "books";
+    }
 
 }
