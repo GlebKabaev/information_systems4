ca5f650b566a483a56e0a5b1190cfc665bc39fec
Gleb <glebkabaev200307@gmail.com>
2024-11-26 13:13:57 +0300

    getBook() deleteBook() init

--- src/main/java/com/glb/practice/lab_4/controller/RestBookWebController.java
+++ src/main/java/com/glb/practice/lab_4/controller/RestBookWebController.java
@@ -1,5 +1,10 @@
 package com.glb.practice.lab_4.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.glb.practice.lab_4.model.Book;
 import com.glb.practice.lab_4.model.BookModel;
+import com.glb.practice.lab_4.model.ShortBook;
 
 @RestController
 @RequestMapping("/api/v1/books")
-public class RestBookController {
+public class RestBookWebController {
     BookModel bookModel = BookModel.getInstance();
-    @GetMapping("/{id}")
+    @GetMapping({"/",""})
+    public String getBooks(Model model) {
+        List<ShortBook> shortBookList= bookModel.get_k_n_shortList(0,30);
+        String stringShortBookList = shortBookList.stream()
+                .map(ShortBook::toString)
+                .collect(Collectors.joining("\n"));
+        return stringShortBookList;
+    }
+    @GetMapping({"/{id}","/{id}/"})
     public String getBook(@PathVariable int id) {
         
         System.out.println("Requested book ID: " + id);
@@ -24,4 +38,9 @@ public class RestBookController {
         System.out.println("Found book: " + book);
         return book.toJson();
     }
+    @DeleteMapping({"/delete/{id}","/delete/{id}/"})
+    public void deleteBook(@PathVariable int id){
+        bookModel.deleteBookById(id);
+    }
+
 }
